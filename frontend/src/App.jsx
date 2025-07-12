f0b7e9a2abbf358419bf9289e7e7d58d49a402d3
tsuhani-19 <163666350+tsuhani-19@users.noreply.github.com>
2025-07-12 11:57:30 +0530

    Reslved the issue in App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Generate from './pages/Generate';
-import Publish from './pages/Publish';
+
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/generate" element={<Generate />} />
-        <Route path="/publish" element={<Publish />} />
+        
       </Routes>
     </Router>
   );
